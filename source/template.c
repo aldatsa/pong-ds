957f7e6459600c9e956499b7ab0065e1046c3e3f
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2015-01-01 21:16:12 +0100

    Draw the left paddle

--- source/template.c
+++ source/template.c
@@ -28,8 +28,18 @@ int main(void) {
        unsigned char height;
        unsigned char width;
     } ball;
+
+    typedef struct {
+        unsigned char x;
+        unsigned char y;
+        char speed;
+        unsigned char height;
+        unsigned char width;
+    } paddle;
     
     ball b = {SCREEN_WIDTH / 2 - 1 - 4, SCREEN_HEIGHT / 2 - 1 - 4, 1, 1, 8, 8};
+    
+    paddle p1 = {0, SCREEN_HEIGHT / 2 - 1 - 16, 1, 32, 8};
     
 	videoSetMode(MODE_0_2D);
 	videoSetModeSub(MODE_0_2D);
@@ -41,6 +51,7 @@ int main(void) {
 	oamInit(&oamSub, SpriteMapping_1D_32, false);
 
 	u16* gfx = oamAllocateGfx(&oamMain, SpriteSize_8x8, SpriteColorFormat_256Color);
+    u16* gfx_p1 = oamAllocateGfx(&oamMain, SpriteSize_8x32, SpriteColorFormat_256Color);
 	u16* gfxSub = oamAllocateGfx(&oamSub, SpriteSize_16x16, SpriteColorFormat_256Color);
 
 	for(i = 0; i < b.height * b.width / 2; i++)
@@ -49,6 +60,11 @@ int main(void) {
 		gfxSub[i] = 1 | (1 << 8);
 	}
 
+    for(i = 0; i < p1.height * p1.width / 2; i++)
+	{
+		gfx_p1[i] = 1 | (1 << 8);
+	}
+    
 	SPRITE_PALETTE[1] = RGB15(31,31,31);    // White
 	SPRITE_PALETTE_SUB[1] = RGB15(0,31,0);
 
@@ -98,7 +114,21 @@ int main(void) {
 			false	//apply mosaic
 			);              
 		
-		
+		oamSet(&oamMain, //main graphics engine context
+			1,           //oam index (0 to 127)  
+			p1.x, p1.y,   //x and y pixle location of the sprite
+			0,                    //priority, lower renders last (on top)
+			0,					  //this is the palette index if multiple palettes or the alpha value if bmp sprite	
+			SpriteSize_8x32,     
+			SpriteColorFormat_256Color, 
+			gfx_p1,                  //pointer to the loaded graphics
+			-1,                  //sprite rotation data  
+			false,               //double the size when rotating?
+			false,			//hide the sprite?
+			false, false, //vflip, hflip
+			false	//apply mosaic
+			);
+        
 		oamSet(&oamSub, 
 			0, 
 			touch.px, 
